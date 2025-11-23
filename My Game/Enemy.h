4e21aeed0abb9045618ef04c9fd469a4acb69293
Kay30000 <khayes102001@gmail.com>
2025-11-23 15:29:19 -0600

    Enemy files added

---
+++ My Game/Enemy.h
@@ -0,0 +1,21 @@
+//Enemy.h
+#pragma once
+#include "Object.h"
+#include "EventTimer.h"
+
+class CEnemy : public CObject
+{
+public:
+    CEnemy(const Vector2& pos);
+    ~CEnemy();
+
+    void Update(float dt);   // override OK
+    void Initialize();
+
+    void TakeDamage(int damage); // declare TakeDamage so other code can call it
+
+private:
+    float m_fHealth = 0.0f; // health for the enemy
+    LEventTimer* m_pFrameEvent = nullptr;
+    LEventTimer* m_pAttackCooldown = nullptr;
+};
